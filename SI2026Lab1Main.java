1ddcebd9bc168daa2c80a99fcfa6236c04438017
sofija0808 <sofijastojanovska2005@gmail.com>
2026-03-27 15:38:27 +0100

    Implement book search

--- SI2026Lab1Main.java
+++ SI2026Lab1Main.java
@@ -52,9 +52,14 @@ class Library {
     }
 
     // TODO: Implement in branch feature-search-books
-    public boolean searchBookByTitle(String title) {
-        return false;
+   public boolean searchBookByTitle(String title) {
+    for (Book book : books) {
+        if (book.getTitle().equalsIgnoreCase(title)) {
+            return true;
+        }
     }
+    return false;
+}
 
     // TODO: Implement in branch feature-borrow-book
     public void borrowBook(String title) {
@@ -120,5 +125,7 @@ public class SI2026Lab1Main {
         library.addBook(new Book("1984", "George Orwell", "Dystopian"));
 
         System.out.println("Library initialized.");
+
+        System.out.println(library.searchBookByTitle("The Hobbit"));
     }
 }
